eed3eaca5e8830849da94e53b8e7689314a128ac
aerohero <aerohero@icloud.com>
2025-01-24 20:29:06 +0900

    circl progressbar 구현

--- TimerDemo/TimerDemo/TimerDemoApp.swift
+++ TimerDemo/TimerDemo/TimerDemoApp.swift
@@ -12,6 +12,15 @@ struct TimerDemoApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    setWindowFloating() // toggle 등을 사용해 기능을 on/off 할 수 있도록 수정할 필요가 있다.
+                }
+        }
+    }
+    
+    func setWindowFloating() {
+        if let window = NSApplication.shared.windows.first {
+            window.level = .floating
         }
     }
 }
